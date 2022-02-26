7b10cb93e35e7f81631790899a2910b99478f680
tokeniacnet <80170039+tokencenter@users.noreply.github.com>
2022-02-26 21:30:13 +0300

    Update Home.vue

--- .vuepress/components/Home.vue
+++ .vuepress/components/Home.vue
@@ -80,7 +80,7 @@
                     <b-embed
                             type="iframe"
                             aspect="16by9"
-                            src="https://docs.google.com/forms/d/e/1FAIpQLSefU7E64lPdo4ZVrKS9yjCXzQ0PwEo37SgkwpIPUJ3CiRlrfA/viewform?embedded=true"
+                            src="#"
                             allow="accelerometer;autoplay;clipboard-write;encrypted-media;gyroscope;picture-in-picture"
                             allowfullscreen
                     ></b-embed>
